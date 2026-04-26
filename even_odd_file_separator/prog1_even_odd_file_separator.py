ea16ccc13b469b7c5de729f57d994ad700feeb4a
Tristanjr <jrtristanmelvin@gmail.com>
2026-04-27 02:22:30 +0800

    check each number using modulo operation to determine whether it is even or odd

--- even_odd_file_separator/prog1_even_odd_file_separator.py
+++ even_odd_file_separator/prog1_even_odd_file_separator.py
@@ -3,3 +3,20 @@ class number_separator:
         self.numbers_file = "numbers.txt"
         self.even_file = "even.txt"
         self.odd_file = "odd.txt"
+
+    def separate_numbers(self):
+        try:
+            with open(self.numbers_file, "r") as file:
+                numbers = file.read().split()
+
+            even_numbers = []
+            odd_numbers = []
+
+            for num in numbers:
+                number = int(num)
+
+                if number % 2 == 0:
+                    even_numbers.append(str(number))
+                else:
+                    odd_numbers.append(str(number))
+
