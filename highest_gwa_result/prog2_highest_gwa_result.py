a2eeb51e26b7c4dcfe5f72a69962859acdc909f9
Tristanjr <jrtristanmelvin@gmail.com>
2026-04-27 22:20:41 +0800

    read student records from file

--- highest_gwa_result/prog2_highest_gwa_result.py
+++ highest_gwa_result/prog2_highest_gwa_result.py
@@ -1,3 +1,8 @@
 class StudentGWA:
     def __init__(self):
         self.file_name = "students.txt"
+
+    def highest_gwa(self):
+        try:
+            with open(self.file_name, "r") as file:
+                students = file.readlines()
