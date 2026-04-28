6f8077e5d54aa2ab4ca5f8aa97beeddd7928315b
Tristanjr <jrtristanmelvin@gmail.com>
2026-04-28 15:14:10 +0800

    implement logic to compute squares of evens and cubes of odds

--- integer_file_processor/prog4_integer_file_processor.py
+++ integer_file_processor/prog4_integer_file_processor.py
@@ -7,3 +7,19 @@ class IntegerProcessor:
     def read_numbers(self):
         with open(self.file_name, "r") as file:
             return file.read().split()
+        
+    def process_numbers(self):
+        numbers = self.read_numbers()
+
+        even_squares = []
+        odd_cubes = []
+
+        for num in numbers:
+            n = int(num)
+
+            if n % 2 == 0:
+                even_squares.append(str(n ** 2))  # square of even numbers
+            else:
+                odd_cubes.append(str(n ** 3))     # cube of odd numbers
+
+        return even_squares, odd_cubes
