bdd9fd131d3068be355afc631ab84e1766aea222
Tristanjr <jrtristanmelvin@gmail.com>
2026-04-28 13:39:38 +0800

    add write_lines method with append file saving

--- mylife_file_writer/prog3_mylife_file_writer.py
+++ mylife_file_writer/prog3_mylife_file_writer.py
@@ -1,4 +1,15 @@
 class JournalWriter:
     def __init__(self):
         self.file_name = "mylife.txt"
-        
+
+        def write_lines(self):
+        with open(self.file_name, "a") as file:
+            while True:
+                line = input("Enter line: ")
+                file.write(line + "\n")
+
+                more = input("Are there more lines y/n? ").lower()
+
+                if more == "n":
+                    print("Lines have been added to mylife.txt")
+                    break
