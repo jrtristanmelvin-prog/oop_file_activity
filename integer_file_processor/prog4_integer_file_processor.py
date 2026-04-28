7c7c67a37fd0bfc817fd5b081ca723bc55aa32aa
Tristanjr <jrtristanmelvin@gmail.com>
2026-04-28 15:12:53 +0800

    add method to read integers from input file

--- integer_file_processor/prog4_integer_file_processor.py
+++ integer_file_processor/prog4_integer_file_processor.py
@@ -3,4 +3,7 @@ class IntegerProcessor:
         self.file_name = "integer_file_processor/integers.txt"
         self.double_file = "double.txt"
         self.triple_file = "triple.txt"
-        
+
+    def read_numbers(self):
+        with open(self.file_name, "r") as file:
+            return file.read().split()
