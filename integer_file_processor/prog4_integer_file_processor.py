fbb9f59421cd8e3a489d6c6e3bb27b045236fb4c
Tristanjr <jrtristanmelvin@gmail.com>
2026-04-28 15:11:18 +0800

    create IntegerProcessor class structure

---
+++ integer_file_processor/prog4_integer_file_processor.py
@@ -0,0 +1,6 @@
+class IntegerProcessor:
+    def __init__(self):
+        self.file_name = "integer_file_processor/integers.txt"
+        self.double_file = "double.txt"
+        self.triple_file = "triple.txt"
+        
