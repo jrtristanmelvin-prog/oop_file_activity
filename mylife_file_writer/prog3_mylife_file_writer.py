8ea1bd027417bf1bc437f0304156034737bc5d74
Tristanjr <jrtristanmelvin@gmail.com>
2026-04-29 00:19:43 +0800

    save mylife.txt output in mylife_file_writer folder

--- mylife_file_writer/prog3_mylife_file_writer.py
+++ mylife_file_writer/prog3_mylife_file_writer.py
@@ -1,6 +1,12 @@
+import os
+
 class JournalWriter:
     def __init__(self):
-        self.file_name = "mylife.txt"
+        self.folder = "mylife_file_writer"
+        self.file_name = os.path.join(self.folder, "mylife.txt")
+
+        # Ensure the folder exists
+        os.makedirs(self.folder, exist_ok=True)
 
     def write_lines(self):
         with open(self.file_name, "a") as file:
@@ -11,7 +17,7 @@ class JournalWriter:
                 more = input("Are there more lines y/n? ").lower()
 
                 if more == "n":
-                    print("Lines have been added to mylife.txt")
+                    print(f"Lines have been added to {self.file_name}")
                     break
 
 writer = JournalWriter()
